pls0x001/mapbox-gl-native

commit 4d5c6333be52aae4a9c72f4b01941e16ead503f4

[core] move retry logic to DefaultFileSource

src/mbgl/storage/default_file_source.cpp
@@ -23,22 +23,22 @@
 #include <algorithm>
 #include <cassert>
 
-
 namespace algo = boost::algorithm;
 
 namespace mbgl {
 
 DefaultFileSource::DefaultFileSource(FileCache* cache, const std::string& root)
-    : thread(std::make_unique<util::Thread<Impl>>(util::ThreadContext{"FileSource", util::ThreadType::Unknown, util::ThreadPriority::Low}, cache, root)) {
+    : thread(std::make_unique<util::Thread<Impl>>(
+          util::ThreadContext{ "FileSource", util::ThreadType::Unknown, util::ThreadPriority::Low },
+          cache,
+          root)) {
 }
 
 DefaultFileSource::~DefaultFileSource() {
     MBGL_VERIFY_THREAD(tid);
 }
 
-Request* DefaultFileSource::request(const Resource& resource,
-                                    uv_loop_t* l,
-                                    Callback callback) {
+Request* DefaultFileSource::request(const Resource& resource, uv_loop_t* l, Callback callback) {
     assert(l);
 
     if (!callback) {
@@ -74,7 +74,7 @@ Request* DefaultFileSource::request(const Resource& resource,
     return req;
 }
 
-void DefaultFileSource::cancel(Request *req) {
+void DefaultFileSource::cancel(Request* req) {
     assert(req);
     req->cancel();
     thread->invoke(&Impl::cancel, req);
@@ -90,56 +90,37 @@ DefaultFileSource::Impl::Impl(FileCache* cache_, const std::string& root)
       httpContext(HTTPContextBase::createContext(loop)) {
 }
 
-DefaultFileRequest* DefaultFileSource::Impl::find(const Resource& resource) {
-    const auto it = pending.find(resource);
-    if (it != pending.end()) {
-        return &it->second;
-    }
-    return nullptr;
-}
-
 void DefaultFileSource::Impl::add(Request* req) {
-    const Resource& resource = req->resource;
-    DefaultFileRequest* request = find(resource);
+    auto& request = pending.emplace(req->resource, req->resource).first->second;
 
-    if (!request) {
-        request = &pending.emplace(resource, resource).first->second;
-    }
+    // Trigger a potentially required refresh of this Request
+    update(request);
 
     // Add this request as an observer so that it'll get notified when something about this
     // request changes.
-    request->observers.insert(req);
-
-    update(request);
-
-    if (request->response) {
-        // We've got a response, so send the (potentially stale) response to the requester.
-        req->notify(request->response);
-    }
+    request.addObserver(req);
 }
 
-void DefaultFileSource::Impl::update(DefaultFileRequest* request) {
-    if (request->response) {
+void DefaultFileSource::Impl::update(DefaultFileRequest& request) {
+    if (request.getResponse()) {
         // We've at least obtained a cache value, potentially we also got a final response.
         // The observers have been notified already; send what we have to the new one as well.
 
-        // Before returning the existing response, make sure that it is still fresh.
-        if (!request->response->stale && request->response->isExpired()) {
-            // Create a new Response object with `stale = true`, but the same data, and
-            // replace the current request object we have.
-            auto response = std::make_shared<Response>(*request->response);
-            response->stale = true;
-            request->response = response;
-        }
+        // Before returning the existing response, make sure that it is still fresh, or update the
+        // `stale` flag.
+        request.checkResponseFreshness();
 
-        if (request->response->stale && !request->realRequest) {
+        if (request.getResponse()->stale && !request.realRequest) {
             // We've returned a stale response; now make sure the requester also gets a fresh
             // response eventually. It's possible that there's already a request in progress.
             // Note that this will also trigger updates to all other existing listeners.
             // Since we already have data, we're going to verify
-            startRealRequest(request, request->response);
+            startRealRequest(request);
+        } else {
+            // The response is still fresh (or there's already a request for refreshing the resource
+            // in progress), so there's nothing we need to do.
         }
-    } else if (!request->cacheRequest && !request->realRequest) {
+    } else if (!request.cacheRequest && !request.realRequest) {
         // There is no request in progress, and we don't have a response yet. This means we'll have
         // to start the request ourselves.
         if (cache) {
@@ -152,60 +133,71 @@ void DefaultFileSource::Impl::update(DefaultFileRequest* request) {
     }
 }
 
-void DefaultFileSource::Impl::startCacheRequest(DefaultFileRequest* request) {
+void DefaultFileSource::Impl::startCacheRequest(DefaultFileRequest& request) {
     // Check the cache for existing data so that we can potentially
     // revalidate the information without having to redownload everything.
-    request->cacheRequest = cache->get(request->resource, [this, request](std::shared_ptr<Response> response) {
-        request->cacheRequest = nullptr;
-        if (response) {
-            response->stale = response->isExpired();
-        }
+    request.cacheRequest =
+        cache->get(request.resource, [this, &request](std::shared_ptr<Response> response) {
+            request.cacheRequest = nullptr;
+            if (response) {
+                response->stale = response->isExpired();
+                request.setResponse(response);
+            }
 
-        if (!response || response->stale) {
-            // No response or stale cache. Run the real request.
-            startRealRequest(request, response);
-        }
+            if (!response || response->stale) {
+                // No response or stale cache. Run the real request.
+                startRealRequest(request);
+            }
 
-        if (response) {
             // Notify in all cases; requestors can decide whether they want to use stale responses.
-            notify(request, response, FileCache::Hint::No);
-        }
-    });
+            request.notify();
+
+            reschedule(request);
+        });
 }
 
-void DefaultFileSource::Impl::startRealRequest(DefaultFileRequest* request, std::shared_ptr<const Response> response) {
+void DefaultFileSource::Impl::startRealRequest(DefaultFileRequest& request) {
     // Cancel the timer if we have one.
-    if (request->timerRequest) {
-        request->timerRequest->stop();
+    if (request.timerRequest) {
+        request.timerRequest->stop();
     }
 
-    auto callback = [request, this] (std::shared_ptr<const Response> res, FileCache::Hint hint) {
-        request->realRequest = nullptr;
-        notify(request, res, hint);
+    auto callback = [this, &request](std::shared_ptr<const Response> response) {
+        request.realRequest = nullptr;
+
+        if (cache) {
+            // Store response in database. Make sure we only refresh the expires column if the data
+            // didn't change.
+            FileCache::Hint hint = FileCache::Hint::Full;
+            if (request.getResponse() && response->data == request.getResponse()->data) {
+                hint = FileCache::Hint::Refresh;
+            }
+            cache->put(request.resource, response, hint);
+        }
+
+        request.setResponse(response);
+        request.notify();
+        reschedule(request);
     };
 
-    if (algo::starts_with(request->resource.url, "asset://")) {
-        request->realRequest = assetContext->createRequest(request->resource, callback, loop, assetRoot);
+    if (algo::starts_with(request.resource.url, "asset://")) {
+        request.realRequest =
+            assetContext->createRequest(request.resource, callback, loop, assetRoot);
     } else {
-        request->realRequest = httpContext->createRequest(request->resource, callback, loop, response);
+        request.realRequest =
+            httpContext->createRequest(request.resource, callback, loop, request.getResponse());
     }
 }
 
 void DefaultFileSource::Impl::cancel(Request* req) {
-    DefaultFileRequest* request = find(req->resource);
-
-    if (request) {
+    auto it = pending.find(req->resource);
+    if (it != pending.end()) {
         // If the number of dependent requests of the DefaultFileRequest drops to zero,
         // cancel the request and remove it from the pending list.
-        request->observers.erase(req);
-        if (request->observers.empty()) {
-            if (request->cacheRequest) {
-                request->cacheRequest.reset();
-            }
-            if (request->realRequest) {
-                request->realRequest->cancel();
-            }
-            pending.erase(request->resource);
+        auto& request = it->second;
+        request.removeObserver(req);
+        if (!request.hasObservers()) {
+            pending.erase(it);
         }
     } else {
         // There is no request for this URL anymore. Likely, the request already completed
@@ -217,42 +209,134 @@ void DefaultFileSource::Impl::cancel(Request* req) {
     req->destruct();
 }
 
-void DefaultFileSource::Impl::notify(DefaultFileRequest* request, std::shared_ptr<const Response> response, FileCache::Hint hint) {
-    // First, remove the request, since it might be destructed at any point now.
-    assert(find(request->resource) == request);
-    assert(response);
+void DefaultFileSource::Impl::reschedule(DefaultFileRequest& request) {
+    if (request.realRequest) {
+        // There's already a request in progress; don't start another one.
+        return;
+    }
 
-    // Notify all observers.
-    request->response = response;
-    for (auto req : request->observers) {
+    const auto timeout = request.getRetryTimeout();
+
+    if (timeout == 0) {
+        update(request);
+    } else if (timeout > 0) {
+        if (!request.timerRequest) {
+            request.timerRequest = std::make_unique<uv::timer>(util::RunLoop::getLoop());
+        }
+
+        // timeout is in seconds, but the timer takes milliseconds.
+        request.timerRequest->start(1000 * timeout, 0, [this, &request] {
+            assert(!request.realRequest);
+            startRealRequest(request);
+        });
+    }
+}
+
+// ----- DefaultFileRequest -----
+
+DefaultFileRequest::~DefaultFileRequest() {
+    if (realRequest) {
+        realRequest->cancel();
+        realRequest = nullptr;
+    }
+    // timerRequest and cacheRequest are automatically canceld upon destruction.
+}
+
+void DefaultFileRequest::addObserver(Request* req) {
+    observers.insert(req);
+
+    if (response) {
+        // We've got a response, so send the (potentially stale) response to the requester.
         req->notify(response);
     }
+}
 
-    if (cache) {
-        // Store response in database
-        cache->put(request->resource, response, hint);
-    }
+void DefaultFileRequest::removeObserver(Request* req) {
+    observers.erase(req);
+}
 
-    // Set timer for requests that have a known expiry times. Expiry times of 0 are technically
-    // expiring immediately, but we can't continually request.
-    if (!request->realRequest && response->expires > 0) {
-        const int64_t now = std::chrono::duration_cast<std::chrono::seconds>(
-                                    SystemClock::now().time_since_epoch()).count();
-        const int64_t timeout = response->expires - now;
+bool DefaultFileRequest::hasObservers() const {
+    return !observers.empty();
+}
 
-        if (timeout <= 0) {
-            update(request);
-        } else {
-            if (!request->timerRequest) {
-                request->timerRequest = std::make_unique<uv::timer>(util::RunLoop::getLoop());
-            }
-
-            // timeout is in seconds, but the timer takes milliseconds.
-            request->timerRequest->start(1000 * timeout, 0, [this, request] {
-                update(request);
-            });
+void DefaultFileRequest::notify() {
+    if (response) {
+        for (auto req : observers) {
+            req->notify(response);
         }
     }
 }
 
+void DefaultFileRequest::setResponse(const std::shared_ptr<const Response>& response_) {
+    response = response_;
+
+    if (response->error) {
+        failedRequests++;
+    } else {
+        // Reset the number of subsequent failed requests after we got a successful one.
+        failedRequests = 0;
+    }
 }
+
+const std::shared_ptr<const Response>& DefaultFileRequest::getResponse() const {
+    return response;
+}
+
+int64_t DefaultFileRequest::getRetryTimeout() const {
+    if (!response) {
+        // If we don't have a response, we should retry immediately.
+        return 0;
+    }
+
+    // A value < 0 means that we should not retry.
+    int64_t timeout = -1;
+
+    if (response->error) {
+        assert(failedRequests > 0);
+        switch (response->error->reason) {
+        case Response::Error::Reason::Server: {
+            // Retry immediately, unless we have a certain number of attempts already
+            const int graceRetries = 3;
+            if (failedRequests <= graceRetries) {
+                timeout = 1;
+            } else {
+                timeout = 1 << std::min(failedRequests - graceRetries, 32);
+            }
+        } break;
+        case Response::Error::Reason::Connection: {
+            // Exponential backoff
+            timeout = 1 << std::min(failedRequests - 1, 32);
+        } break;
+        default:
+            // Do not retry due to error.
+            break;
+        }
+    }
+
+    // Check to see if this response expires earlier than a potential error retry.
+    if (response->expires > 0) {
+        const int64_t expires =
+            response->expires -
+            std::chrono::duration_cast<std::chrono::seconds>(SystemClock::now().time_since_epoch())
+                .count();
+        // Only update the timeout if we don't have one yet, and only if the new timeout is shorter
+        // than the previous one.
+        timeout = timeout < 0 ? expires : std::min(timeout, std::max<int64_t>(0, expires));
+    }
+
+    return timeout;
+}
+
+void DefaultFileRequest::checkResponseFreshness() {
+    if (response && !response->stale && response->isExpired()) {
+        // Create a new Response object with `stale = true`, but the same data, and
+        // replace the current request object we have.
+        // We're not immediately swapping the member variable because it's declared as `const`, and
+        // we first have to update the `stale` flag.
+        auto staleResponse = std::make_shared<Response>(*response);
+        staleResponse->stale = true;
+        response = staleResponse;
+    }
+}
+
+} // namespace mbgl

include/mbgl/storage/file_cache.hpp
@@ -16,7 +16,7 @@ class FileCache : private util::noncopyable {
 public:
     virtual ~FileCache() = default;
 
-    enum class Hint : uint8_t { Full, Refresh, No };
+    enum class Hint : bool { Full, Refresh };
     using Callback = std::function<void(std::unique_ptr<Response>)>;
 
     virtual std::unique_ptr<WorkRequest> get(const Resource &resource, Callback callback) = 0;

include/mbgl/storage/response.hpp
@@ -8,20 +8,47 @@ namespace mbgl {
 
 class Response {
 public:
+    Response() = default;
+    Response(const Response&);
+    Response& operator=(const Response&);
+
     bool isExpired() const;
 
 public:
-    enum Status { Error, Successful, NotFound };
+    class Error;
+    // When this object is empty, the response was successful.
+    std::unique_ptr<const Error> error;
 
-    Status status = Error;
+    // Stale responses are fetched from cache and are expired.
     bool stale = false;
-    std::string message;
+
+    // The actual data of the response. This is guaranteed to never be empty.
+    std::shared_ptr<const std::string> data;
+
     int64_t modified = 0;
     int64_t expires = 0;
     std::string etag;
-    std::shared_ptr<const std::string> data;
 };
 
-}
+class Response::Error {
+public:
+    enum class Reason : uint8_t {
+        // Success = 1, // Reserve 1 for Success.
+        NotFound = 2,
+        Server = 3,
+        Connection = 4,
+        Canceled = 5,
+        Other = 6,
+    } reason = Reason::Other;
+
+    // An error message from the request handler, e.g. a server message or a system message
+    // informing the user about the reason for the failure.
+    std::string message;
+
+public:
+    Error(Reason, const std::string& = "");
+};
+
+} // namespace mbgl
 
 #endif

commit 4e148dc55fad0cdae27a8fd5f4f2659ba064f102

[core] Fixed bit-shifting error in exponential backoff
This change fixes a bug introduced in 4d5c6333be52aae4a9c72f4b01941e16ead503f4 for #2727. 1 << 32 would exceed the size of int64_t. The static analyzer reported undefined behavior here because std::min() might or might not return 32.

src/mbgl/storage/default_file_source.cpp
@@ -325,12 +325,12 @@ int64_t DefaultFileRequest::getRetryTimeout() const {
             if (failedRequests <= graceRetries) {
                 timeout = 1;
             } else {
-                timeout = 1 << std::min(failedRequests - graceRetries, 32);
+                timeout = 1 << std::min(failedRequests - graceRetries, 31);
             }
         } break;
         case Response::Error::Reason::Connection: {
             // Exponential backoff
-            timeout = 1 << std::min(failedRequests - 1, 32);
+            timeout = 1 << std::min(failedRequests - 1, 31);
         } break;
         default:
             // Do not retry due to error.