pls0x001/mapbox-gl-native

commit 9c9ff2d2d4aeac9f605a6ca23447e54adea9f6bd

[ios, osx] Updated build system for jazzy
Consolidated NSValue categories to work around realm/jazzy#539. Documented MGLErrorCode. Moved jazzy configuration file into platform/ios/. Removed redundant include/Mapbox.h that was only being used by jazzy, now that the static frameworks’s umbrella header is based on that of the dynamic framework. Moved framework/Mapbox.h to include/Mapbox.h for consistency.

platform/ios/jazzy.yml
@@ -15,10 +15,11 @@ umbrella_header: platform/ios/include/Mapbox.h
 framework_root: platform/darwin/include
 
 custom_categories:
-  - name: Map
+  - name: Maps
     children:
       - MGLAccountManager
       - MGLMapCamera
+      - MGLMapDebugMaskOptions
       - MGLMapView
       - MGLMapViewDelegate
       - MGLStyle
@@ -37,7 +38,7 @@ custom_categories:
       - MGLOverlay
       - MGLShape
       - MGLUserLocation
-  - name: Offline Storage
+  - name: Offline Maps
     children:
       - MGLOfflineRegion
       - MGLOfflineStorage
@@ -54,7 +55,6 @@ custom_categories:
       - MGLOfflinePackState
       - MGLOfflinePackStateUserInfoKey
       - MGLTilePyramidOfflineRegion
-      - NSValue(MGLOfflinePackAdditions)
   - name: Geometry
     children:
       - MGLClockDirectionFormatter
@@ -73,4 +73,3 @@ custom_categories:
       - MGLDegreesFromRadians
       - MGLRadiansFromDegrees
       - MGLStringFromCoordinateBounds
-      - NSValue(MGLGeometryAdditions)

platform/ios/scripts/document.sh
@@ -33,6 +33,7 @@ rm -rf ${OUTPUT}
 mkdir -p ${OUTPUT}
 
 jazzy \
+    --config platform/ios/jazzy.yml
     --sdk iphonesimulator \
     --swift-version $SWIFT_VERSION \
     --github-file-prefix https://github.com/mapbox/mapbox-gl-native/tree/${BRANCH} \

platform/darwin/include/MGLGeometry.h
@@ -100,48 +100,6 @@ NS_INLINE CLLocationDegrees MGLDegreesFromRadians(CGFloat radians) {
  */
 @interface NSValue (MGLGeometryAdditions)
 
-/**
- Creates a new value object containing the specified Core Location geographic
- coordinate structure.
- 
- @param coordinate The value for the new object.
- @return A new value object that contains the geographic coordinate information.
- */
-+ (instancetype)valueWithMGLCoordinate:(CLLocationCoordinate2D)coordinate;
-
-/**
- The Core Location geographic coordinate structure representation of the value.
- */
-@property (readonly) CLLocationCoordinate2D MGLCoordinateValue;
-
-/**
- Creates a new value object containing the specified Mapbox coordinate span
- structure.
- 
- @param span The value for the new object.
- @return A new value object that contains the coordinate span information.
- */
-+ (instancetype)valueWithMGLCoordinateSpan:(MGLCoordinateSpan)span;
-
-/**
- The Mapbox coordinate span structure representation of the value.
- */
-@property (readonly) MGLCoordinateSpan MGLCoordinateSpanValue;
-
-/**
- Creates a new value object containing the specified Mapbox coordinate bounds
- structure.
- 
- @param bounds The value for the new object.
- @return A new value object that contains the coordinate bounds information.
- */
-+ (instancetype)valueWithMGLCoordinateBounds:(MGLCoordinateBounds)bounds;
-
-/**
- The Mapbox coordinate bounds structure representation of the value.
- */
-@property (readonly) MGLCoordinateBounds MGLCoordinateBoundsValue;
-
 @end
 
 NS_ASSUME_NONNULL_END

platform/darwin/include/MGLOfflinePack.h
@@ -177,25 +177,4 @@ typedef struct MGLOfflinePackProgress {
 
 @end
 
-/**
- Methods for round-tripping `MGLOfflinePackProgress` values.
- */
-@interface NSValue (MGLOfflinePackAdditions)
-
-/**
- Creates a new value object containing the given `MGLOfflinePackProgress`
- structure.
- 
- @param progress The value for the new object.
- @return A new value object that contains the offline pack progress information.
- */
-+ (NSValue *)valueWithMGLOfflinePackProgress:(MGLOfflinePackProgress)progress;
-
-/**
- The `MGLOfflinePackProgress` structure representation of the value.
- */
-@property (readonly) MGLOfflinePackProgress MGLOfflinePackProgressValue;
-
-@end
-
 NS_ASSUME_NONNULL_END

platform/darwin/include/MGLTypes.h
@@ -17,10 +17,15 @@ NS_ASSUME_NONNULL_BEGIN
 /** Indicates an error occurred in the Mapbox SDK. */
 extern NSString * const MGLErrorDomain;
 
+/** Error constants for the Mapbox SDK. */
 typedef NS_ENUM(NSInteger, MGLErrorCode) {
+    /** An unknown error occurred. */
     MGLErrorCodeUnknown = -1,
+    /** The resource could not be found. */
     MGLErrorCodeNotFound = 1,
+    /** The connection received an invalid server response. */
     MGLErrorCodeBadServerResponse = 2,
+    /** An attempt to establish a connection failed. */
     MGLErrorCodeConnectionFailed = 3,
 };
 

platform/darwin/include/NSValue+MGLAdditions.h
@@ -1,0 +1,75 @@
+#import <Foundation/Foundation.h>
+
+#import "MGLGeometry.h"
+#import "MGLOfflinePack.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ Methods for round-tripping values for Mapbox-defined types.
+ */
+@interface NSValue (MGLAdditions)
+
+#pragma mark Working with Geographic Coordinate Values
+
+/**
+ Creates a new value object containing the specified Core Location geographic
+ coordinate structure.
+ 
+ @param coordinate The value for the new object.
+ @return A new value object that contains the geographic coordinate information.
+ */
++ (instancetype)valueWithMGLCoordinate:(CLLocationCoordinate2D)coordinate;
+
+/**
+ The Core Location geographic coordinate structure representation of the value.
+ */
+@property (readonly) CLLocationCoordinate2D MGLCoordinateValue;
+
+/**
+ Creates a new value object containing the specified Mapbox coordinate span
+ structure.
+ 
+ @param span The value for the new object.
+ @return A new value object that contains the coordinate span information.
+ */
++ (instancetype)valueWithMGLCoordinateSpan:(MGLCoordinateSpan)span;
+
+/**
+ The Mapbox coordinate span structure representation of the value.
+ */
+@property (readonly) MGLCoordinateSpan MGLCoordinateSpanValue;
+
+/**
+ Creates a new value object containing the specified Mapbox coordinate bounds
+ structure.
+ 
+ @param bounds The value for the new object.
+ @return A new value object that contains the coordinate bounds information.
+ */
++ (instancetype)valueWithMGLCoordinateBounds:(MGLCoordinateBounds)bounds;
+
+/**
+ The Mapbox coordinate bounds structure representation of the value.
+ */
+@property (readonly) MGLCoordinateBounds MGLCoordinateBoundsValue;
+
+#pragma mark Working with Offline Map Values
+
+/**
+ Creates a new value object containing the given `MGLOfflinePackProgress`
+ structure.
+ 
+ @param progress The value for the new object.
+ @return A new value object that contains the offline pack progress information.
+ */
++ (NSValue *)valueWithMGLOfflinePackProgress:(MGLOfflinePackProgress)progress;
+
+/**
+ The `MGLOfflinePackProgress` structure representation of the value.
+ */
+@property (readonly) MGLOfflinePackProgress MGLOfflinePackProgressValue;
+
+@end
+
+NS_ASSUME_NONNULL_END

platform/darwin/src/MGLCoordinateFormatter.m
@@ -1,6 +1,6 @@
 #import "MGLCoordinateFormatter.h"
 
-#import "MGLGeometry.h"
+#import "NSValue+MGLAdditions.h"
 
 @implementation MGLCoordinateFormatter {
     NSNumberFormatter *_numberFormatter;

platform/darwin/src/MGLGeometry.mm
@@ -44,38 +44,3 @@ double MGLZoomLevelForAltitude(CLLocationDistance altitude, CGFloat pitch, CLLoc
     CGFloat mapPixelWidthAtZoom = std::cos(MGLRadiansFromDegrees(latitude)) * mbgl::util::M2PI * mbgl::util::EARTH_RADIUS_M / metersPerPixel;
     return ::log2(mapPixelWidthAtZoom / mbgl::util::tileSize);
 }
-
-@implementation NSValue (MGLGeometryAdditions)
-
-+ (instancetype)valueWithMGLCoordinate:(CLLocationCoordinate2D)coordinate {
-    return [self valueWithBytes:&coordinate objCType:@encode(CLLocationCoordinate2D)];
-}
-
-- (CLLocationCoordinate2D)MGLCoordinateValue {
-    CLLocationCoordinate2D coordinate;
-    [self getValue:&coordinate];
-    return coordinate;
-}
-
-+ (instancetype)valueWithMGLCoordinateSpan:(MGLCoordinateSpan)span {
-    return [self valueWithBytes:&span objCType:@encode(MGLCoordinateSpan)];
-}
-
-- (MGLCoordinateSpan)MGLCoordinateSpanValue {
-    MGLCoordinateSpan span;
-    [self getValue:&span];
-    return span;
-}
-
-+ (instancetype)valueWithMGLCoordinateBounds:(MGLCoordinateBounds)bounds {
-    return [self valueWithBytes:&bounds objCType:@encode(MGLCoordinateBounds)];
-}
-
-- (MGLCoordinateBounds)MGLCoordinateBoundsValue {
-    MGLCoordinateBounds bounds;
-    [self getValue:&bounds];
-    return bounds;
-}
-
-@end
-

commit 3355ff41749c9954795e7c0853a8cac648e6832a

[ios] Fixed jazzy build step
Fixed some regressions introduced in 9c9ff2d2d4aeac9f605a6ca23447e54adea9f6bd for #4802. A backslash was missing from a shell script, causing arguments to get dropped and output to go in the repo root. Paths in the jazzy configuration file needed to be updated so jazzy could find the headers.

platform/ios/jazzy.yml
@@ -11,8 +11,8 @@ head: |
 objc: Yes
 skip_undocumented: Yes
 hide_documentation_coverage: Yes
-umbrella_header: platform/ios/include/Mapbox.h
-framework_root: platform/darwin/include
+umbrella_header: src/Mapbox.h
+framework_root: ../darwin/src
 
 custom_categories:
   - name: Maps

platform/ios/scripts/document.sh
@@ -33,7 +33,7 @@ rm -rf ${OUTPUT}
 mkdir -p ${OUTPUT}
 
 jazzy \
-    --config platform/ios/jazzy.yml
+    --config platform/ios/jazzy.yml \
     --sdk iphonesimulator \
     --swift-version $SWIFT_VERSION \
     --github-file-prefix https://github.com/mapbox/mapbox-gl-native/tree/${BRANCH} \