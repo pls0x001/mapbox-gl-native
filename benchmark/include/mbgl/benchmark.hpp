gomapi/gomapi-sdk-ios

[build] Mark explicitly visible symbols
Easier to maintain than a linker script. We build
everything with visibility hidden by default.

benchmark/include/mbgl/benchmark.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <mbgl/util/util.hpp>
+
 namespace mbgl {
 
-int runBenchmark(int argc, char* argv[]);
+MBGL_PUBLIC int runBenchmark(int argc, char* argv[]);
 
 } // namespace mbgl

test/include/mbgl/test.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <mbgl/util/util.hpp>
+
 namespace mbgl {
 
-int runTests(int argc, char* argv[]);
+MBGL_PUBLIC int runTests(int argc, char* argv[]);
 
 } // namespace mbgl

pls0x001/mapbox-gl-native

benchmark/include/mbgl/benchmark.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <mbgl/util/util.hpp>
+
 namespace mbgl {
 
-int runBenchmark(int argc, char* argv[]);
+MBGL_EXPORT int runBenchmark(int argc, char* argv[]);
 
 } // namespace mbgl

test/include/mbgl/test.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <mbgl/util/util.hpp>
+
 namespace mbgl {
 
-int runTests(int argc, char* argv[]);
+MBGL_EXPORT int runTests(int argc, char* argv[]);
 
 } // namespace mbgl